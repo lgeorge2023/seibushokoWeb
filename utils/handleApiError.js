7c1011a11924aa65e14d993913f3c5b94bcb87d5
Anandhu <anandhu.asokan@calpinetech.com>
2024-04-04 11:24:54 +0530

    api 500 error commented

--- utils/handleApiError.js
+++ utils/handleApiError.js
@@ -1,19 +1,18 @@
-// utils/apiUtils.js
-
 import { notifications } from "@mantine/notifications";
 
 export const handleApiError = (error, router, t) => {
-
+  
   if (error === 404) {
     notifications.show({
       title: t('Error'),
       message: t('ERROR_OCCURED'),
       color: 'red',
     });
-
-  } else if (error === 500) {
-    router.push('/500');
-  } else {
+  }
+  //  else if (error == '500') {
+  //   router.push('/500');
+  // }
+   else {
     notifications.show({
       title: t('Error'),
       message: t(error?.trim()),
